46d5ea0e490466f3509edde0db6280f5c174325c
Dfidelis23 <simoesdaniel139@gmail.com>
2025-07-23 10:54:33 -0300

    Add files via upload

--- ex01/script.js
+++ ex01/script.js
@@ -1,28 +1,29 @@
 function soma(){
     var numero1 = document.getElementById("n1").valueAsNumber
     var numero2 = document.getElementById("n2").valueAsNumber
-    //console.log(numero1, numero2)
     var resposta = document.getElementById('resposta').textContent = numero1 + numero2
 }
 
 function subtracao(){
     var numero1 = document.getElementById("n3").valueAsNumber
     var numero2 = document.getElementById("n4").valueAsNumber
-    //console.log(numero1, numero2)
     var resposta = document.getElementById('sub').textContent = numero1 - numero2
 }
 
 function multiplicacao(){
-    var numero1 = document.getElementById("n5").valueAsNumber
-    var numero2 = document.getElementById("n6").valueAsNumber
-    //console.log(numero1, numero2)
+    var numero1 = parseFloat(document.getElementById("n5").value)
+    var numero2 = parseFloat(document.getElementById("n6").value)
     var resposta = document.getElementById('mul').textContent = numero1 * numero2
 }
 
 function divisao(){
-    var numero1 = document.getElementById("n7").valueAsNumber
-    var numero2 = document.getElementById("n8").valueAsNumber
-    //console.log(numero1, numero2)
-    var resposta = document.getElementById('div').textContent = numero1 / numero2
+    var numero1 = parseFloat(document.getElementById("n7").value)
+    var numero2 = parseFloat(document.getElementById("n8").value)
+    var resposta = document.getElementById('div')
+    if(numero2 !== 0){
+        resposta.textContent = numero1 / numero2
+    }else{
+        resposta.textContent = "Não se divide por 0"
+    }
 }
 
